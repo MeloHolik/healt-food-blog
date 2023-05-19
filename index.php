d17a0d6d96058eb2273967fbff7f5e9abfaa8a53
Cherry-puff <131194369+Cherry-puff@users.noreply.github.com>
2023-05-19 21:55:24 +0300

    Add files via upload
    Навигация между файлами. Калькулятор КБЖУ, добавить рецепты, обновить имеющиеся, авторизация и регистрация (проверяю еще, чтобы все корректно работало). Также база данных для phpmyadmin через openserver.

--- index.php
+++ index.php
@@ -15,7 +15,7 @@ echo '</pre>';
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>health-food-blog</title>
     <link rel="stylesheet" href="styles.css">
-    <style>
+     <style>
         img {
             width: 500px;
             height: auto;
@@ -27,9 +27,19 @@ echo '</pre>';
     </style>
 </head> 
 <body>
-  <header>
-  	 <h1>Блог о здоровой еде</h1>
-  </header>
+<header>
+    <h1>Блог о здоровой еде</h1> 
+</header> 
+<nav> 
+    <ul> 
+        <li>
+            <a href="index.html">Главная</a>
+        </li> <li><a href="calculator.php">Калькулятор КБЖУ</a></li> 
+        <li><a href="new_recipe.php">Добавить блюдо</a></li> 
+        <li><a href="update.php">Редактировать блюдо</a></li> 
+        <li><a href="authorization.php">Вход</a></li>
+    </ul> 
+    </nav> 
   <?php
   foreach($recept as $num) {
   	?>
@@ -40,26 +50,23 @@ echo '</pre>';
 Жиры: <?= $num[5] ?><br>
 Углеводы: <?=$num[6] ?><br>
 Калории: <?=$num[7] ?><br>
-    <img><?= $num[9]?></img>
     <img><?php  
-    if ($num[11]) {
-  echo "<img src='img/$num[11]'/>";;
+    if ($num[9]) {
+  echo "<img src='img/$num[9]'/>";;
 } else { 
      echo $recept=''; 
  } ?></img><br>
     <h2>Ингредиенты:</h2>
-    <?= $num[8]
-     ?><br>
-    <img><?= $num[10]?></img>
+    <?= $num[8]?><br>
     <img><?php  
-    if ($num[12]) {
-  echo "<img src='img/$num[12]'/>";;
+    if ($num[10]) {
+  echo "<img src='img/$num[10]'/>";;
 } else { 
      echo $recept=''; 
  } ?></img>
 <br>
     <h2>Рецепт</h2>    
-    <?= $num[3] ?><br>
+  <p align="left"><?= $num[3]?></p><br>
     </p>
 <p><a href="update.php?id=<?= $num[0] ?>">Редактировать рецепт</a></p>
 <?php
@@ -86,11 +93,11 @@ echo '</pre>';
     </li>
 </ul>
 <p> Изображение блюда</p>
-<input type='file' name='img_main_download'><br>
+<input type='file' name='img_main_download' value="<?= $recept['img_main_download']?>"><br>
 <p>Ингредиенты</p>
 <textarea name='ingredients' size="80"></textarea>
 <p>Изображение ингредиентов</p>
-<input type='file' name='img_sub_download'>
+<input type='file' name='img_sub_download' value="<?= $recept['img_sub_download']?>">
 <p> Рецепт </p>
 <textarea name='instructions' size="80"></textarea>
 <button type='submit' >Отправить рецепт</button>

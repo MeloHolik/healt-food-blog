2e858ee8976a55d535f65f842136aa0f7d12e4c7
Cherry-puff <131194369+Cherry-puff@users.noreply.github.com>
2023-05-22 17:49:47 +0300

    Add files via upload
    Добавил стили ко всем страницам, подвал сайта и навигацию. Файлы index.html, style.css не влияют на другие страницы. Считаю проект завершенным по тому, что успели обсудить еще на созвонах с куратором. Много недостатков, в том числе по стилю, недоработанному функционалу и возможностям дальнейшего продвижения сайта. 
    Спасибо за "активное участие". 
    Опыт полезный и изучать все и реализовывать в одиночку было трудной задачей, но выполнимой.

--- calculate.php
+++ calculate.php
@@ -27,44 +27,46 @@ if ($_POST["activity"] == "sedentary") {
 }
 
 ?>
-<!DOCTYPE html> 
+<!DOCTYPE html>
 <html lang="ru">
-<meta charset="UTF-8">
+<head>
+    <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0"> 
-<head> 
-	<title>Калькулятор КБЖУ</title> 
-</head>
-<nav> 
-    <ul> 
-        <li>
-            <a href="index.html">Главная</a>
-        </li> <li><a href="calculate.php">Калькулятор КБЖУ</a></li> 
-        <li><a href="new_recipes.php">Добавить блюдо</a></li> 
-        <li><a href="update.php">Редактировать блюдо</a></li> 
-        <li><a href="authorization.php">Вход</a></li>
-    </ul> 
-    </nav>  
-<body>
-  <h1>Калькулятор КБЖУ</h1>
-  <form action="calculate.php" method="post">
-    <label for="gender">Пол:</label>
-    <select id="gender" name="gender">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Калькулятор КБЖУ</title>
+    <link rel="stylesheet" href="styles.css">
+</head> 
+<body style="background-color: #f2f2f2; font-family: Arial, sans-serif; padding: 20px;">
+  <header>
+    <h1>Калькулятор КБЖУ</h1>
+  </header>
+ <nav style="background-color: #fff; padding: 20px;">
+  <ul style="list-style: none; margin: 0; padding: 0; display: flex; justify-content: space-between; font-family: Arial, sans-serif;">
+    <li><a href="index.php" style="text-transform: uppercase; text-decoration: none; color: #333; font-size: 18px; font-weight: bold; transition: all 0.3s ease;">Главная</a></li>
+    <li><a href="calculate.php" style="text-transform: uppercase; text-decoration: none; color: #333; font-size: 18px; font-weight: bold; transition: all 0.3s ease;">Калькулятор КБЖУ</a></li>
+    <li><a href="new_recipe.php" style="text-transform: uppercase; text-decoration: none; color: #333; font-size: 18px; font-weight: bold; transition: all 0.3s ease;">Добавить блюдо</a></li>
+    <li><a href="update.php" style="text-transform: uppercase; text-decoration: none; color: #333; font-size: 18px; font-weight: bold; transition: all 0.3s ease;">Редактировать блюдо</a></li>
+    <li><a href="authorization.php" style="text-transform: uppercase; text-decoration: none; color: #fff; font-size: 18px; font-weight: bold; background-color: #c3272c; padding: 10px 20px; border-radius: 3px; transition: all 0.3s ease;">Вход</a></li>
+  </ul>
+</nav>
+  <form action="calculate.php" method="post" style="background-color: #fff; padding: 20px; border-radius: 5px;">
+    <label for="gender" style="font-size: 18px; font-weight: bold; color: #333; display: block;">Пол:</label>
+    <select id="gender" name="gender" style="font-size: 16px; padding: 8px; border: 1px solid #ccc; border-radius: 3px; margin-bottom: 10px;">
       <option value="male">Мужской</option>
       <option value="female">Женский</option>
     </select><br>
 
-    <label for="weight">Вес (кг):</label>
-    <input type="number" id="weight" name="weight"><br>
+    <label for="weight" style="font-size: 18px; font-weight: bold; color: #333; display: block;">Вес (кг):</label>
+    <input type="number" id="weight" name="weight" style="font-size: 16px; padding: 8px; border: 1px solid #ccc; border-radius: 3px; margin-bottom: 10px;"><br>
 
-    <label for="height">Рост (см):</label>
-    <input type="number" id="height" name="height"><br>
+    <label for="height" style="font-size: 18px; font-weight: bold; color: #333; display: block;">Рост (см):</label>
+    <input type="number" id="height" name="height" style="font-size: 16px; padding: 8px; border: 1px solid #ccc; border-radius: 3px; margin-bottom: 10px;"><br>
 
-    <label for="age">Возраст:</label>
-    <input type="number" id="age" name="age"><br>
+    <label for="age" style="font-size: 18px; font-weight: bold; color: #333; display: block;">Возраст:</label>
+    <input type="number" id="age" name="age" style="font-size: 16px; padding: 8px; border: 1px solid #ccc; border-radius: 3px; margin-bottom: 10px;"><br>
 
-    <label for="activity">Степень активности:</label>
-    <select id="activity" name="activity">
+    <label for="activity" style="font-size: 18px; font-weight: bold; color: #333; display: block;">Степень активности:</label>
+    <select id="activity" name="activity" style="font-size: 16px; padding: 8px; border: 1px solid #ccc; border-radius: 3px; margin-bottom: 10px;">
       <option value="sedentary">Минимальная активность (сидячий образ жизни)</option>
       <option value="lightly-active">Легкая активность (упражнения 1-3 раза в неделю)</option>
       <option value="moderately-active">Средняя активность (упражнения 3-5 раз в неделю)</option>
@@ -72,25 +74,35 @@ if ($_POST["activity"] == "sedentary") {
       <option value="extra-active">Очень высокая активность (упражнения каждый день)</option>
     </select><br>
 
-    <button type="submit">Рассчитать КБЖУ</button>
+    <button type="submit" style="font-size: 18px; padding: 10px 20px; border: none; border-radius: 3px; background-color: #c3272c; color: #fff; cursor: pointer;">Рассчитать КБЖУ</button>
   </form>
+  <?php if 
+(isset($_POST["weight"])) 
+{ 
+  echo '<h2 style="text-align:center;">Ваша норма калорий:</h2>'; 
+  echo '<p style="text-align:center;"><strong>' . round($calories, 2) . '</strong> ккал/день</p>';
+   $protein = round($weight * 2.2, 2); 
+   $fat = round($calories * 0.25 / 9, 2); 
+   $carbs = round(($calories - ($protein * 4) - ($fat * 9)) / 4, 2); 
+   echo '<h3 style="text-align:center;">Распределение макронутриентов:</h3>'; 
+   echo '<p style="text-align:center;">Белки: <strong>' . $protein . '</strong> г</p>'; 
+   echo '<p style="text-align:center;">Жиры: <strong>' . $fat . '</strong> г</p>'; 
+   echo '<p style="text-align:center;">Углеводы: <strong>' . $carbs . '</strong> г</p>'; $water=round($weight * 30, 2); 
+   echo '<h3 style="text-align:center;">Необходимое количество воды:</h3>'; 
+   echo '<p style="text-align:center;"><strong>' . $water . '</strong> мл/день</p>'; } ?>
+<footer style="background-color: #222; color: #fff; padding: 32px;">
+  <div style="max-width: 1200px; margin: 0 auto;">
+    <div style="display: flex; justify-content: space-between; align-items: center;">
+      <h4 style="font-size: 24px; margin: 0;">БЛОГ О ЗДОРОВОЙ ЕДЕ</h4>
+      <ul style="list-style: none; margin: 0; padding: 0; display: flex;">
+        <li style="margin-right: 16px;"><a href="#" style="color: #fff; text-decoration: none; font-size: 16px;">Главная</a></li>
+        <li style="margin-right: 16px;"><a href="calculate.php" style="color: #fff; text-decoration: none; font-size: 16px;">Калькулятор КБЖУ</a></li>
+        <li><a href="authorization.php" style="color: #fff; text-decoration: none; font-size: 16px;">Войти</a></li>
+      </ul>
+    </div>
+    <p style="font-size: 16px; margin-top: 16px;">Весь материал на сайте является авторским, не является лекарственной рекомендацией и не заменяет консультацию специалиста.</p>
+    <p style="font-size: 16px; margin-top: 8px;">© 2023 Блог о здоровой еде. Все права защищены.</p>
+  </div>
+</footer>
 </body>
 </html>
-<?php
-  if (isset($_POST["weight"])) {
-    echo "<h2>Ваша норма калорий:</h2>";
-    echo "<p><strong>" . round($calories, 2) . "</strong> ккал/день</p>";
-
-    $protein = round($weight * 2.2, 2); 
-    $fat = round($calories * 0.25 / 9, 2); 
-    $carbs = round(($calories - ($protein * 4) - ($fat * 9)) / 4, 2);
-    echo "<h3>Распределение макронутриентов:</h3>";
-echo "<p>Белки: <strong>" . $protein . "</strong> г</p>";
-echo "<p>Жиры: <strong>" . $fat . "</strong> г</p>";
-echo "<p>Углеводы: <strong>" . $carbs . "</strong> г</p>";
-
-$water=round($weight * 30, 2); 
-echo "<h3>Необходимое количество воды:</h3>";
-echo "<p><strong>" . $water . "</strong> мл/день</p>";
-} 
-?>
